a6659fad3add37ce9fc8f606fbd5b2a56e6e409c
mrudulks <devmrudulks@gmail.com>
2022-09-21 00:50:31 +0530

    intial commit

---
+++ pages/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="main-container container">
+      <div class="cardlist card mb-2" v-for="item in todoList" :key="item.id">
+        <p class="text-danger">{{ item.title }}</p>
+          <div class="sub-cards card mb-2" v-for="card in item.cards" :key="card.id">
+            <p class="text-success">{{ card.title }}</p>
+          </div>
+          <div>
+          <input type="text" v-model="subCard.title">
+          <button @click="addCard(item.id)">Add Card</button>
+          </div>
+          
+      </div>
+      <div class="add-btn">
+      <input type="text" v-model="newItem.title" >
+      <button class="" type="save-btn" @click="saveTodo()">Add</button>
+      <button type="button">Add todo</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data(){
+    return{
+      newItem:{},
+      subCard:{},
+      todoList:[
+        {
+          id:1,
+          title:"Books",
+          cards:[
+            {
+              id:1,
+              title:"Books card1",
+              description:"test descriptions"
+            },
+            {
+              id:2,
+              title:"Books card2",
+              description:"test descriptions"
+            }
+          ]
+        },
+        {
+          id:2,
+          title:"Books 2",
+          cards:[
+            {
+              id:1,
+              title:"Books card1",
+              description:"test descriptions"
+            },
+            {
+              id:2,
+              title:"Books card2",
+              description:"test descriptions"
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods:{
+    saveTodo(){
+      var listLength = this.todoList.length;
+      var id = this.todoList[listLength - 1].id + 1;
+      this.newItem.id = id;
+      this.todoList.push(this.newItem);
+      this.newItem = {}
+    },
+    addCard(id){
+      this.todoList.map((v) =>{
+        if(v.id == id){
+          if( v?.cards){
+            // var cardLength = v.cards.length;
+            console.log("no cards")
+          }
+          // this.subCard.id = v.cards[cardLength -1 ].id + 1;
+          // v.cards.push(this.subCard);
+          // this.subCard={}
+        }
+      })
+    }
+  },
+  mounted(){
+
+  }
+}
+</script>
+<style>
+body{
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ pages/page2.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="main-wrapper">
+    <div class="main-container container pt-5">
+      <div class="grid-view">
+        <div class="todo-list-wrapper" v-for="item in workSpaceList" :key="item.id">
+          <div class="todo-contents">
+            <div class="todo-header">
+              <p>{{ item.name }}</p>
+            </div>
+            <input type="text" v-model="item.newCardName">
+            <button @click="createColumn(item)">Add coulmn</button>
+            <div class="todo-add-card">
+                <ul>
+                    <li v-for="card in item.items" :key="card.id">
+                        {{ card.name }}
+                    </li>
+                </ul>
+            </div>
+          </div>
+        </div>
+        <div class="workspace-add">
+          <div class="input-field">
+            <input type="text" v-model="workspaceName">
+            <button class="btn btn-primary" @click="createWorkspace()">Save</button>
+          </div>
+          <div class="">
+            <p>Add Another List</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        workspaceName: '',
+        workSpaceList: [],
+        newItem: ''
+      }
+    },
+    methods: {
+      createWorkspace() {
+        var id = Math.floor((Math.random() * 1000));
+        this.workSpaceList.push({
+          id: id,
+          name: this.workspaceName,
+          newCardName:'',
+          items:[]
+        })
+      },
+      createColumn(column) {
+        const id = Math.floor((Math.random() * 1000));
+        column.items.push({
+            id:id,
+            name:column.newCardName        })
+      }
+    },
+  }
+
+</script>
+<style>
+  body {
+    background: #41b3A4;
+  }
+
+  .grid-view {
+    display: flex;
+    gap: 1rem;
+  }
+
+  .todo-list-wrapper {
+    height: 100%;
+    width: 275px;
+    background-color: #adadad;
+    padding: 1rem;
+    border-radius: .5rem;
+    overflow: hidden;
+    display: flex;
+  }
+
+  .todo-header p {
+    font-size: 1.2rem;
+    font-weight: 700;
+    color: #557a95;
+  }
+
+</style>
